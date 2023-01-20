6a97e64d1d07f8abd8b01bcd2f0162d1e1c4f31f
jefersonmadera <120031553+jefersonmadera@users.noreply.github.com>
2023-01-20 00:26:37 -0500

    algoritmia

--- src/Probando/com/Ifmayormenos.java
+++ src/Probando/com/Ifmayormenos.java
@@ -1,5 +1,28 @@
 package Probando.com;
 
-public class Ifmayormenos {
+import java.util.Scanner;
 
-}
+public class Ifmayormenos {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("write three numbers ");
+        int num1 = entrada.nextInt();
+        int num2 = entrada.nextInt();
+        int num3 = entrada.nextInt();
+        if ((num1>num2) && (num2>num3) ){
+            System.out.println("el orden es " + num1 + num2 + num3);
+        }else if ((num1<num2) && (num2<num3)){
+            System.out.println("el orden es " + num3 + num2 + num1);
+        }else if ((num3>num1) && (num2>num3)){
+            System.out.println("el orden es " + num2 + num3+num1);
+        }else if ((num1>num3)&&(num2>num1)){
+            System.out.println("el orden es " + num2 + num1 + num3);
+        } else if ((num1>num3)&&( num3>num2)) {
+            System.out.println("el orden es " + num1 + num3 + num2);
+        }else {
+            System.out.println("el orden es " + num3 + num1 + num2);
+        }
+
+        }
+    }
+

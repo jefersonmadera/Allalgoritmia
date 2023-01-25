548831d69757879789f651c5bf0471ab9c9bf47c
jefersonmadera <120031553+jefersonmadera@users.noreply.github.com>
2023-01-25 16:55:03 -0500

    algoritmia

--- src/Probando/com/pedirfor.java
+++ src/Probando/com/pedirfor.java
@@ -1,4 +1,22 @@
 package Probando.com;
 
+import java.util.Scanner;
+
 public class pedirfor {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+        int suma=0;
+        int numero;
+        // este codigo pide 10 numeros y luego los suma
+        System.out.println("digita los numeros que vas a sumar \n");
+        for(int i=1;i<=10;i++){
+            System.out.println("numero " + i );
+            System.out.println("====================");
+            numero= entrada.nextInt();
+            suma+=numero;
+
+        }
+        System.out.println("el resultado de la suma es = " + suma );
+
+    }
 }

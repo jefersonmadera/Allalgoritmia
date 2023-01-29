05ccd2a74a014ba9ceebedf878554b4b82eca386
jefersonmadera <120031553+jefersonmadera@users.noreply.github.com>
2023-01-28 19:31:11 -0500

    Arreglosanother

---
+++ src/Ejercicios/com/Anther_WayArrays.java
@@ -0,0 +1,10 @@
+package Ejercicios.com;
+
+public class Anther_WayArrays {
+    public static void main(String[] args) {
+        int [] numeros  = {5,7,4};
+        for(int i =0; i<3;i++){
+            System.out.println(numeros[i]);
+        }
+    }
+}

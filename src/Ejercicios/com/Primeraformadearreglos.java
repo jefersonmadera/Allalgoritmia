b52cf6d1a463593d07c2c1205304f55fd2e0c8e5
jefersonmadera <120031553+jefersonmadera@users.noreply.github.com>
2023-01-28 19:18:03 -0500

    Arreglos

---
+++ src/Ejercicios/com/Primeraformadearreglos.java
@@ -0,0 +1,13 @@
+package Ejercicios.com;
+
+public class Primeraformadearreglos {
+    public static void main(String[] args) {
+        int [] numeros = new int[3];
+        numeros [0]=5;
+        numeros [1]=7;
+        numeros [2]=4;
+        for (int i =0 ; i<3;i++){
+            System.out.println(numeros[i]);
+        }
+    }
+}
